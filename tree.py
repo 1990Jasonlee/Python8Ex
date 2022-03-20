e20512bf909e871f6f9a80341e2ca1dfef3362a9
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-20 13:41:39 -0400

    update

--- tree.py
+++ tree.py
@@ -1,3 +1,10 @@
-def write_file(file_path, content):
-    fout = open(file_path, 'w')
-    fout.write(content)
+import os
+
+# def write_file(file_path, content):
+#     fout = open(file_path, 'w')
+#     fout.write(content)
+
+def file(name, files):
+    result = os.listdir(name)
+    for item in result:
+        path = os.path.join(name, result)
