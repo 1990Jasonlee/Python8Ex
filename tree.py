4efb9f66e6f8e12254ac0f3e1f531b3da5a22706
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-20 20:03:18 -0400

    complete

--- tree.py
+++ tree.py
@@ -5,6 +5,13 @@ import os
 #     fout.write(content)
 
 def file(name, files):
+    #list all
     result = os.listdir(name)
+
+    #check all in path and write in file
     for item in result:
-        path = os.path.join(name, result)
+        path = os.path.join(name, item)
+        if os.path.isfile(path):
+            files.write(path)
+        else:
+            file(path, files)
